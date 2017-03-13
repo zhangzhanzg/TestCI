bb7e8925db5c8c552ad708c0cd76d339e726220d
zhangzhan <zhangzhan@pe.vc>
2017-03-13 15:50:11 +0800

    工程初始化

---
+++ AngelCrunch/AngelCrunch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AngelCrunch
+//
+//  Created by Angel on 2017/3/13.
+//  Copyright © 2017年 Angel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
